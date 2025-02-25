c73901082f331c455c17c2492c7a64db7bca88ce
Nathan Carrillo <157448953+n8-carr@users.noreply.github.com>
2025-02-24 19:53:47 -0800

    Update userProfile.h

--- userProfile.h
+++ userProfile.h
@@ -1,1 +1,21 @@
+// includes notification preferences class
 
+class userProfile {
+
+private: 
+// attributes
+string Username;
+string Email;
+notificationPreferences NotificationPreferences;
+
+public:
+// operations/methods 
+
+//constructor
+userProfile(string Username, string Email, notificationPreferences NotificationPreferences);
+
+// getter functions for private attributes
+string get_User();
+string get_Email();
+notificationPreferences get_NotifPref();
+}
