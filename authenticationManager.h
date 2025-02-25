94c1d25f4a32073ba0bae2538f579885b4cdd424
Nathan Carrillo <157448953+n8-carr@users.noreply.github.com>
2025-02-24 20:20:24 -0800

    Update authenticationManager.h

--- authenticationManager.h
+++ authenticationManager.h
@@ -1,1 +1,24 @@
+public enum registrationStatus {
+  SUCCESS,
+  USER_ALREADY_EXISTS,
+  UNKNOWN_ERROR
+}
+
+// includes userProfile and registrationStatus
+class authenticationManager{
+//no attributes
+
+private:
+
+bool validateUsername(string username);
+// bool for valid/invalid username
+bool validatePassoword(string password);
+// bool for valid/invalud password
+string hashPassword(string password);
+bool validateEmail(string email);
+// bool for valid/invalid email
+registrationStatus saveNewUser(userProfile UserProfile, string password);
+// returns from options listed above
+}
+
 
